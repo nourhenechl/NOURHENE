e126f49b734a273af2b01ea6d373b21badd2fa95
nour hene26 <51926779+nourhenechl@users.noreply.github.com>
2021-03-16 17:05:30 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+﻿<?php
+session_start();
+
+$date=date('F d, Y, g:i:s a' );
+
+echo "TODAY is  " .$date;
+	
+	if(!isset($_SESSION["username"])){
+		header("Location: login.php");
+		exit(); 
+	}
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+	<link rel="stylesheet" href="style.css" />
+	</head>
+	<body>
+		<div class="sucess">
+		<h1>Bienvenue <?php echo $_SESSION['username']; ?>!</h1>
+		        <a href="http://172.16.10.9/" >
+			
+			<h3>" Click here !"</h3>
+			
+		</a>   
+		<a href="logout.php">Déconnexion</a>
+		</div>
+	</body>
+</html>
---
+++ logintime.php
@@ -0,0 +1,8 @@
+<?php 
+
+   
+$login_tym = date('Y-m-d H:i:s'); 
+ 
+$logout_tym=date('Y-m-d  H:i:s'); 
+ 
+?> 
